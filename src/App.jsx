118bf605c368609064d86b107c3ee5b80f5bc3e4
Micheal Mac Donnacha <micheal.macdonnacha13@gmail.com>
2024-09-04 20:58:42 +0100

    Adding react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom'
 import Atbash from './ciphers/Atbash'
 import ROT13 from './ciphers/ROT13'
 import Navbar from './components/Navbar'
@@ -6,11 +7,24 @@ import Navbar from './components/Navbar'
 function App() { 
   
   return(
-    <>
-      <Atbash />
+    <BrowserRouter basename="/classic-cryptography/">
+      <header>
+        <nav>
+          <h1>Classic Cryptography</h1>
+          <NavLink to="atbash">Atbash</NavLink>
+          <NavLink to="rot13">ROT13</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="atbash" element={<Atbash />} />
+          <Route path="rot13" element={<ROT13 />} />
+        </Routes>
+      </main>
+      {/* <Atbash />
       <hr />
-      <ROT13 />
-    </>
+      <ROT13 /> */}
+    </BrowserRouter>
   )
 }
 
